3dc93b9daf3a9ebb1ba24e2164c59941954f19a0
Francois Poizat <francois.poizat@gmail.com>
2021-03-31 16:10:08 +0200

    fix nlohmann/json issues with boost::filesystem::path parsing

--- src/lib/engine/engine/dataformat/json.h
+++ src/lib/engine/engine/dataformat/json.h
@@ -2,4 +2,22 @@
 
 #include <nlohmann/json.hpp>
 
+#include <boost/filesystem.hpp>
+
 using json = nlohmann::json;
+
+namespace nlohmann {
+    template<>
+    struct adl_serializer<boost::filesystem::path>
+    {
+        static void to_json(json& j, const boost::filesystem::path& path)
+        {
+            j = path.string();
+        }
+
+        static void from_json(const json& j, boost::filesystem::path& path)
+        {
+            path = j.get<std::string>();
+        }
+    };
+}
